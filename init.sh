71b9d3b862d5235b97d4518d1b01880e34098677
DukeSpontaneous <eai_bat@tut.by>
2017-07-23 18:29:59 +0300

    update

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
-﻿#!/bin/bash
-﻿ln -s /home/box/web/etc/nginx.conf	/etc/nginx/sites-enabled/test.conf
+﻿#!/bin/sh
+
+/bin/ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
+
+/bin/ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+/etc/init.d/gunicorn restart
