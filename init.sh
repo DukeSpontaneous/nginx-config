0ceab2dba31f77a4b41f74b80a2e19e901b0f85e
DukeSpontaneous <eai_bat@tut.by>
2017-07-23 17:54:01 +0300

    pre

--- init.sh
+++ init.sh
@@ -1,7 +1,3 @@
-﻿sudo ﻿ln -s /home/box/etc/nginx.conf	/etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-
-sudo ln -s /home/box/etc/hello.py	/etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
-
-﻿sudo /etc/init.d/mysql start﻿
+﻿#!/bin/bash
+﻿ln -s /home/box/web/etc/nginx.conf	/etc/nginx/sites-enabled/test.conf
+/etc/init.d/nginx restart
