29fa8d39278d1a3765bd7ff3e356c976fe891d89
DukeSpontaneous <eai_bat@tut.by>
2017-07-23 16:48:26 +0300

    new_dir

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
-﻿sudo ﻿ln -s /home/box/web/etc/nginx.conf	/etc/nginx/sites-enabled/default
+﻿sudo ﻿ln -s /home/box/etc/nginx.conf	/etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/etc/hello.py	/etc/gunicorn.d/hello.py
+sudo ln -s /home/box/etc/hello.py	/etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 
 ﻿sudo /etc/init.d/mysql start﻿
