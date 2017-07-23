4bf5c5834dbe4d8959b155b0e1f0a57bc28f4266
DukeSpontaneous <eai_bat@tut.by>
2017-07-23 13:53:51 +0300

    Gunicorn

---
+++ etc/hello.py
@@ -0,0 +1,5 @@
+﻿bind = "0.0.0.0:8080"
+workers = 4
+pythonpath = "/home/box/web"
+errorlog = "/home/box/web/etc/error.log"
+loglevel = "debug"
---
+++ hello.py
@@ -0,0 +1,6 @@
+﻿def application(environ, start_response):
+	query = environ['QUERY_STRING']
+	start_response('200 OK', [
+		('Content-Type', 'text/plain')
+	])
+	return query.replace("&", "\n")
